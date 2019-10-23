9c2c1dc01a384b3b20e9bd78c64a757883bc4454
Sixto Ortega <sixto_miguel@hotmail.com>
2019-10-23 13:50:53 -0400

    Uploading project

---
+++ ConsoleMQTT/Helpers/MQTTHelper.cs
@@ -0,0 +1,77 @@
+﻿using MQTTnet;
+using MQTTnet.Client.Options;
+using MQTTnet.Extensions.ManagedClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+// Tutorial
+// https://dzone.com/articles/mqtt-publishing-and-subscribing-messages-to-mqtt-b
+
+
+public class MQTTHelper
+{
+    public IManagedMqttClient Client { get; set; }
+    public Action<string> _CallBack;
+
+    public  MQTTHelper(string clientId, string mqttURI, string mqttUser, string mqttPassword, int mqttPort, string topicSubscribe = "", Action<string> CallBack = null, bool mqttSecure = false)
+    {
+        _CallBack = CallBack;
+
+        var messageBuilder = new MqttClientOptionsBuilder()
+            .WithClientId(clientId)
+            .WithCredentials(mqttUser, mqttPassword)
+            .WithTcpServer(mqttURI, mqttPort)
+            .WithCleanSession();
+
+        var options = mqttSecure
+            ? messageBuilder
+              .WithTls()
+              .Build()
+            : messageBuilder
+              .Build();
+
+        var managedOptions = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(options)
+            .Build();
+
+        Client = new MqttFactory().CreateManagedMqttClient();
+        Client.StartAsync(managedOptions);
+
+        if(!string.IsNullOrEmpty(topicSubscribe)) 
+        {
+            var x = this.SubscribeAsync(topicSubscribe).Result;
+        }
+        this.Client.UseApplicationMessageReceivedHandler(e => {
+            string message = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+            _CallBack?.Invoke(message);
+        });
+    }
+
+    public async Task<bool> PublishAsync(string topic, string payload, bool retainFlag = true, int qos = 1)
+    {
+        await Client.PublishAsync(new MqttApplicationMessageBuilder()
+            .WithTopic(topic)
+            .WithPayload(payload)
+            .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+            .WithRetainFlag(retainFlag)
+            .Build());
+        return true;
+    }
+
+    private async Task<bool> SubscribeAsync(string topic, int qos = 1)
+    {
+        await Client.SubscribeAsync(new TopicFilterBuilder()
+            .WithTopic(topic)
+            .WithQualityOfServiceLevel((MQTTnet.Protocol.MqttQualityOfServiceLevel)qos)
+            .Build());
+
+        return true;
+    }
+
+
+}
---
+++ ConsoleMQTT/Program.cs
@@ -0,0 +1,27 @@
+﻿using MQTTnet.Extensions.ManagedClient;
+using System;
+using System.Text;
+
+namespace ConsoleMQTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Para recibir mensaje
+            string topic = "Topic 2";
+            var mqttHelper = new MQTTHelper(Guid.NewGuid().ToString(), "postman.cloudmqtt.com", "lhnrhvok", "FiHorUN2CkCP", 11252, topic, Callback_MessageReceived);
+
+            // Para enviar mensaje
+            //var mqttHelper = new MQTTHelper(Guid.NewGuid().ToString(), "postman.cloudmqtt.com", "lhnrhvok", "FiHorUN2CkCP", 11252);
+            //var x = mqttHelper.PublishAsync("Sixto", "Un nuevo mensaje").Result;
+            Console.ReadLine();
+        }
+
+        static void Callback_MessageReceived(string message)
+        {
+            Console.WriteLine($"Callback_MessageReceived => {message}");
+        }
+
+    }
+}
